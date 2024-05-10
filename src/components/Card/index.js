8969dfc00f84505e00746f7f99bda50f44034972
Flavio-Standard4-UniSJ <flasalesti@gmail.com>
2024-05-10 14:25:40 -0300

    add icone favoritar

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,18 @@
 import { Link } from 'react-router-dom';
 
 import styles from './Card.module.css';
+import favorite from './favorite.png';
+import unfavorite from './unfavorite.png';
 
 function Card({ id }){
     return (
         <section className={ styles.card }>
             <Link to={`/watch/${ id }`}>
-                <img src={`https://img.youtube.com/vi/${ id }/mqdefault.jpg`} alt='capa do vídeo' />
+                <img src={`https://img.youtube.com/vi/${ id }/mqdefault.jpg`} alt='capa do vídeo'  className={ styles.capa } />
             </Link>
+            <figure className={ styles.figure }>
+                <img src={ favorite } alt='curtir ou descurtir vídeo' />
+            </figure>
         </section>
     );
 }
