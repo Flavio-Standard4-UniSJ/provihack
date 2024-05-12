e10112c343bd7d007f45bfa4e4a78a9524a6d56a
Flavio-Standard4-UniSJ <flasalesti@gmail.com>
2024-05-12 13:25:43 -0300

    ajustes na lista de videos add componente loader

--- src/components/SearchVideoList/index.js
+++ src/components/SearchVideoList/index.js
@@ -1,10 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import VideoList from '../VideoList';
 import styles from './SearchVideoList.module.css';
+import Loader from '../Loader';
 
 function SearchVideoList({ videos }){
     const [searchText, setSearchText] = useState("");
     const foundVideos = filterVideos(videos, setSearchText);
+
+    const [ loading, setLoading ] = useState(true);
+    useEffect(()=>{
+        setTimeout(()=> setLoading(false), 500)
+    }, [])
 
     function filterVideos(videos, setSearchText){
         return videos.filter((video)=>video.category.includes(searchText) || video.title.includes(searchText))
@@ -17,10 +23,13 @@ function SearchVideoList({ videos }){
             placeholder='Pesquisar...'
             value={ searchText }
             onChange={ event => setSearchText(event.target.value) } />
-            <VideoList 
-                videos={ foundVideos } 
-                emptyHeading={`Sem vídeos sobre "${ searchText }"`}
-            />
+            {   
+                loading ? <Loader /> :
+                <VideoList 
+                    videos={ foundVideos } 
+                    emptyHeading={`Sem vídeos sobre "${ searchText }"`}
+                />
+            }
         </section>
     );
 }
